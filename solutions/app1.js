f37028cac472f0ad96e41a037bf73672c7a9f0e8
endrearato <endre.arato.81@gmail.com>
2021-11-09 18:22:52 +0100

    done

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,11 @@
 
+const array = [1,2,3,4,5,5,3];
+function removeDuplicatesFromArray() {
+    const uniqueNum = [...new Set(array)];
+    return uniqueNum
+    //console.log(uniqueNum);
+};
+//removeDuplicatesFromArray(array);
 
 
-
-
-
-// export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray;
